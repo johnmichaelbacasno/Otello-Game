d21f84eced72a3f09ff37cf01960d9bbd85a32a3
John Michael <johnmichael.bacasno@g.msuiit.edu.ph>
2022-11-12 21:55:41 +0800

    docs: add docstring in each method in Board class

--- src/utils.py
+++ src/utils.py
@@ -12,98 +12,111 @@ class Board:
     
     @staticmethod
     def copy_board(board):
+        """Returns a shallow copy of the board"""
         return [row[:] for row in board]
     
     @staticmethod
-    def calculate_scores(board, P1, P2):
+    def player_scores(board, P1, P2):
+        "Returns the scores of the two players"
         P1_score, P2_score = 0, 0
-        for x in range(8):
-            for y in range(8):
-                if board[x][y] == P1:
+        for row in range(8):
+            for col in range(8):
+                if board[row][col] == P1:
                     P1_score += 1
-                elif board[x][y] == P2:
+                elif board[row][col] == P2:
                     P2_score += 1
         return P1_score, P2_score
     
     @staticmethod
-    def calculate_score(board, stone):
+    def heuristic_value(board, player):
+        """Returns the heuristic value of a player"""
         score = 0
-        for x in range(8):
-            for j in range(8):
-                if board[x][j] == stone:
+        for row in range(8):
+            for col in range(8):
+                if board[row][col] == player:
                     score += 1
-                elif board[x][j] == Board.opponent_stone(stone):
+                elif board[row][col] == Board.opponent(player):
                     score -= 1
         return score
     
     @staticmethod
-    def in_bounds(row, col):
-        # needed for manual input
+    def final_value(board, player):
+        """Returns final value of a player when the game ends"""
+        score = Board.heuristic_value(board, player)
+        if score < 0:
+            return Board.MIN_SCORE
+        elif score > 0:
+            return Board.MAX_SCORE
+        return score
+    
+    @staticmethod
+    def in_bound(row, col):
+        """Checks if a coordinate is within the board or not"""
         return 0 <= row < 8 and 0 <= col < 8
     
     @staticmethod
     def count_points(board, row, col, horz, vert):
-        stone = board[row][col]
-        count = 1
-        # scalar value allows iteration in (horz, vert) direction
+        """Returns the points"""
+        player = board[row][col]
+        points = 1
         for scalar in range(1, 8 + 1):
-            if Board.in_bounds(row + scalar * horz, col + scalar * vert):
+            if Board.in_bound(row + scalar * horz, col + scalar * vert):
                 if board[row + scalar * horz][col + scalar * vert] == 0:
                     return 0
-                elif board[row + scalar * horz][col + scalar * vert] == stone:
-                    count += 1
+                elif board[row + scalar * horz][col + scalar * vert] == player:
+                    points += 1
                 else:
-                    return count
+                    return points
             else:
                 return 0
-        return count
+        return points
     
     @staticmethod
-    def check_move(board, row, col, stone):
+    def check_move(board, row, col, player):
+        """Returns the points of a player move"""
         points = 0
         directions = [-1, 0, 1]
         for horz in directions:
             for vert in directions:
                 if vert != 0 or horz != 0:
-                    if Board.in_bounds(row + horz, col + vert):
+                    if Board.in_bound(row + horz, col + vert):
                         examined = board[row + horz][col + vert]
-                        if examined != 0 and examined != stone:
+                        if examined != 0 and examined != player:
                             points += Board.count_points(board, row + horz, col + vert, horz, vert)
-                        # move is valid if stones can be flipped
                         if points > 0:
                             return points
         return points
     
     @staticmethod
-    def get_valid_moves(board, stone):
+    def get_valid_moves(board, player):
+        """Returns all valid moves of a player"""
         valid_moves = []
-        for i in range(8):
-            for j in range(8):
-                # explore free spaces only
-                if board[i][j] == 0:
-                    gain = Board.check_move(board, i, j, stone)
-                    if gain > 0:
-                        valid_moves.append(Move((i, j), gain))
+        for row in range(8):
+            for col in range(8):
+                if board[row][col] == 0:
+                    points = Board.check_move(board, row, col, player)
+                    if points > 0:
+                        valid_moves.append(Move((row, col), points))
         if not valid_moves:
             return None
         else:
             return valid_moves
     
-    def transform_board(board, move, stone):
+    @staticmethod
+    def transform_board(board, move, player):
+        """Returns a new instance of the board when a move is applied"""
         if move == None:
             return board
         
         row, col = move
-        
-        # needed to not modify original board
-        board_copy = Board.copy_board(board)
-        board_copy[row][col] = stone
+        new_board = Board.copy_board(board)
+        new_board[row][col] = player
         
         directions = [-1, 0, 1]
         # this combination checks surrounding cells
         for vert in directions:
             for horz in directions:
-                stones = []
+                discs = []
                 if vert == horz == 0:
                     continue
                 
@@ -112,152 +125,147 @@ class Board:
                 lrow += vert
                 lcol += horz
                 
-                while Board.in_bounds(lrow, lcol):
-                    # if stone is opponents color append to flip later
-                    if board_copy[lrow][lcol] != stone and board_copy[lrow][lcol] != 0:
-                        stones.append((lrow, lcol))
-                    # space breaks direct line, no stones flipped
-                    elif board_copy[lrow][lcol] == 0:
+                while Board.in_bound(lrow, lcol):
+                    # if opponent's disc, append to flip later
+                    if new_board[lrow][lcol] != player and new_board[lrow][lcol] != 0:
+                        discs.append((lrow, lcol))
+                    # space breaks direct line, no disc flipped
+                    elif new_board[lrow][lcol] == 0:
                         break
-                    # if same color stone found, flip all stones in between
-                    elif board_copy[lrow][lcol] == stone:
-                        for a, b in stones:
-                            board_copy[a][b] = stone
+                    # if same player disc found, flip all discs in between
+                    elif new_board[lrow][lcol] == player:
+                        for a, b in discs:
+                            new_board[a][b] = player
                         break
                     
                     lrow += vert
                     lcol += horz
         
-        return board_copy
+        return new_board
     
     @staticmethod
-    def is_valid(board, move, stone):
-        gain = Board.check_move(board, *move, stone)
+    def is_valid(board, x, y, player):
+        """Checks if a move is valid or not"""
+        gain = Board.check_move(board, x, y, player)
         return gain > 0
     
     @staticmethod
-    def has_no_move(board, stone):
-        return Board.get_valid_moves(board, stone) == None
-    
-    def final_value(board, stone):
-        # if player wins return max score so adv search uses this branch
-        score = Board.calculate_score(board, stone)
-        if score < 0:
-            return Board.MIN_SCORE
-        elif score > 0:
-            return Board.MAX_SCORE
-        return score
+    def has_no_move(board, player):
+        """Checks if a player has no move or not"""
+        return Board.get_valid_moves(board, player) == None
     
     @staticmethod
-    def opponent_stone(stone):
-        if stone == 1:
+    def opponent(player):
+        """Returns the opponent of a player"""
+        if player == 1:
             return 2
-        elif stone == 2:
+        elif player == 2:
             return 1
         else:
             return 0
     
     @staticmethod
-    def move(board, stone, depth=1):
-        if Board.has_no_move(board, stone):
+    def best_move(board, player, depth=1):
+        """Returns the best move using search algorithm"""
+        if Board.has_no_move(board, player):
             return None
         
-        #move = Board.minimax_search(stone, board, depth)
-        #move = Board.negamax_search(stone, board, depth)
-        move = Board.alphabeta_search(stone, board, Board.MIN_SCORE, Board.MAX_SCORE, depth)
+        # best_move = Board.minimax_search(board, player, depth)
+        # best_move = Board.negamax_search(board, player, depth)
+        best_move = Board.alphabeta_search(board, player, Board.MIN_SCORE, Board.MAX_SCORE, depth)
         
-        return move.coords
+        return best_move
     
     @staticmethod
-    def minimax_search(stone, board, depth):
-        # recursion base case
+    def minimax_search(board, player, depth):
+        """Returns the best move using minimax search"""
         if depth == 0:
-            return Move(None, Board.calculate_score(board, stone))
+            return Move(None, Board.heuristic_value(board, player))
         
-        valid_moves = Board.get_valid_moves(board, stone)
+        valid_moves = Board.get_valid_moves(board, player)
         
-        if stone == 1:
+        if player == 1:
             if not valid_moves:
-                # no more valid moves evaluate oponents next play
+                # if player 1 has no more valid moves, evaluate the player 2's next play
                 if not Board.get_valid_moves(board, 2):
-                    # no more moves for either player, return final state
-                    return Move(None, Board.final_value(board, stone))
-                # opponent has valid moves, return points for that move
-                value = Board.minimax_search(2, board, depth - 1).points 
+                    # if no more moves for both players, return the final move
+                    return Move(None, Board.final_value(board, player))
+                # if player 2 has valid moves, return points for that move
+                value = Board.minimax_search(board, 2, depth - 1).points 
                 return Move(None, value)
             
             best_move = None
             
             for move in valid_moves:
-                move_board = Board.transform_board(board, move.coords, stone)
-                value = Board.minimax_search(2, move_board, depth - 1).points 
+                move_board = Board.transform_board(board, move.coords, player)
+                value = Board.minimax_search(move_board, 2, depth - 1).points 
                 if (best_move is None) or (value > best_move.points):
                     best_move = move
             
             return best_move
         
-        else:
+        elif player == 2:
             if not valid_moves:
-                # no more valid moves evaluate oponents next play
+                # if player 2 has no more valid moves, evaluate the player 1's next play
                 if not Board.get_valid_moves(board, 1):
-                    # no more moves for either player, return final state
-                    return Move(None, Board.final_value(board, stone))
-                # opponent has valid moves, return points for that move
-                value = Board.minimax_search(1, board, depth - 1).points 
+                    # if no more moves for both players, return the final move
+                    return Move(None, Board.final_value(board, player))
+                # if player 1 has valid moves, return points for that move
+                value = Board.minimax_search(board, 1, depth - 1).points 
                 return Move(None, value)
             
             best_move = None
             
             for move in valid_moves:
-                move_board = Board.transform_board(board, move.coords, stone)
-                value = Board.minimax_search(1, move_board, depth - 1).points 
+                move_board = Board.transform_board(board, move.coords, player)
+                value = Board.minimax_search(move_board, 1, depth - 1).points 
                 if (best_move is None) or (value > best_move.points):
                     best_move = move
             
             return best_move
     
     @staticmethod
-    def negamax_search(stone, board, depth):
-        # recursion base case
+    def negamax_search(board, player, depth):
+        """Returns the best move using negamax search"""
         if depth == 0:
-            return Move(None, Board.calculate_score(board, stone))
+            return Move(None, Board.heuristic_value(board, player))
         
-        valid_moves = Board.get_valid_moves(board, stone)
+        valid_moves = Board.get_valid_moves(board, player)
         
         if not valid_moves:
-            # no more valid moves evaluate oponents next play
-            if not Board.get_valid_moves(board, Board.opponent_stone(stone)):
-                # no more moves for either player, return final state
-                return Move(None, Board.final_value(board, stone))
-            # opponent has valid moves, return points for that move
-            value = -Board.minimax_search(Board.opponent_stone(stone), board, depth - 1).points 
+            # if player has no more valid moves, evaluate the opponent's next play
+            if not Board.get_valid_moves(board, Board.opponent(player)):
+                # if no more moves for both players, return the final move
+                return Move(None, Board.final_value(board, player))
+            # if opponent has valid moves, return points for that move
+            value = -Board.minimax_search(board, Board.opponent(player), depth - 1).points 
             return Move(None, value)
         
         best_move = None
         
         for move in valid_moves:
-            move_board = Board.transform_board(board, move.coords, stone)
-            value = -Board.minimax_search(Board.opponent_stone(stone), move_board, depth - 1).points 
+            move_board = Board.transform_board(board, move.coords, player)
+            value = -Board.minimax_search(move_board, Board.opponent(player), depth - 1).points 
             if (best_move is None) or (value > best_move.points):
                 best_move = move
         
         return best_move
     
     @staticmethod
-    def alphabeta_search(stone, board, alpha, beta, depth):
-        # recursion base case
+    def alphabeta_search(board, player, alpha, beta, depth):
+        """Returns the best move using alpha-beta search"""
         if depth == 0:
-            return Move(None, Board.calculate_score(board, stone))
+            return Move(None, Board.heuristic_value(board, player))
         
-        valid_moves = Board.get_valid_moves(board, stone)
+        valid_moves = Board.get_valid_moves(board, player)
         
         if not valid_moves:
-            # no more valid moves evaluate oponents next play
-            if not Board.get_valid_moves(board, Board.opponent_stone(stone)):
-                # no more moves for either player, return final state
-                return Move(None, Board.final_value(board, stone))
-            # opponent has valid moves, return points for that move
-            value = -Board.alphabeta_search(Board.opponent_stone(stone), board, -beta, -alpha, depth - 1).points 
+            # if player has no more valid moves, evaluate the opponent's next play
+            if not Board.get_valid_moves(board, Board.opponent(player)):
+                # if no more moves for both players, return the final move
+                return Move(None, Board.final_value(board, player))
+            # if opponent has valid moves, return points for that move
+            value = -Board.alphabeta_search(board, Board.opponent(player), -beta, -alpha, depth - 1).points 
             return Move(None, value)
         
         best_move = valid_moves[0]
@@ -265,10 +273,10 @@ class Board:
         
         for move in valid_moves:
             if beta <= alpha:
-                # prune nodes that aren't worth visiting
+                # prune nodes that are not worth visiting
                 break
-            move_board = Board.transform_board(board, move.coords, stone)
-            value = -Board.alphabeta_search(Board.opponent_stone(stone), move_board, -beta, -alpha, depth - 1).points 
+            move_board = Board.transform_board(board, move.coords, player)
+            value = -Board.alphabeta_search(move_board, Board.opponent(player), -beta, -alpha, depth - 1).points 
             if value > alpha:
                 # new max
                 alpha = value
